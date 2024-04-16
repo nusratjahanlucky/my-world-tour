7b55e0323ef46e1f4b5111fdffa562f4eaa4e3c3
Nusrat Jahan Lucky <nusratjahanlucky00@gmail.com>
2024-04-16 21:37:27 +0600

    first commit

---
+++ src/components/CountryData/CountryData.jsx
@@ -0,0 +1,12 @@
+// const CountryData = ({country,handleVisitedCountry,handleVisitedFlags}) => {
+    
+//     console.log('inside code data',country,handleVisitedCountry,handleVisitedFlags)
+    
+//     return (
+//         <div>
+//             <h4><small>Country Data:{country.name.common}</small></h4>
+//         </div>
+//     );
+// };
+
+// export default CountryData;
